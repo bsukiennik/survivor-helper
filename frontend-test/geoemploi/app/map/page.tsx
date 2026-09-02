f389b678323debec297d28322b8b1ac5bdbf9111
bsukiennik <bastien.sukiennik@epitech.eu>
2026-09-02 09:23:04 +0200

    add: displaying map

---
+++ frontend-test/geoemploi/app/map/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("./Map"), {
+  ssr: false,
+});
+
+export default function Home() {
+  return (
+    <div>
+      <Map />
+      Hello world!
+    </div>
+  );
+}
